d609b81c3fef8bcbaf354d10719cee025b7ec8a1
iamansin <91902740+iamansin@users.noreply.github.com>
2025-02-06 02:10:04 +0530

    Update testing.py

--- streamlit_webapp/testing.py
+++ streamlit_webapp/testing.py
@@ -7,15 +7,6 @@ import sys
 load_dotenv()
 
 
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-logging.getLogger("neo4j").addHandler(handler)
-logger =logging.getLogger("neo4j").setLevel(logging.DEBUG)
-NEO4J_URI="neo4j+ssc://659de33b.databases.neo4j.io"
-NEO4J_USERNAME="neo4j"
-NEO4J_PASSWORD="GbaCybkq06KMac9xPd5mOKgWbUy5NHawuU4cu91bwIc"
-AURA_INSTANCEID="659de33b"
-AURA_INSTANCENAME="Instance01"
 
 try:
     driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
